da10f3e902982934e2cd71799e2ad27da941e3fa
Alvaro Fernandez <afernandez@s4gaming.com>
2023-12-30 13:15:00 +0100

    add security spring boot

---
+++ src/main/java/local/backend/configurator/SecurityConfig.java
@@ -0,0 +1,50 @@
+package local.backend.configurator;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+//============================= Bean Methods ======================================================
+//============================= ===================================================================
+  @Bean
+  UserDetailsService            userDetailsService(PasswordEncoder encoder) {
+    UserDetails admin = User.withUsername("user")
+        .password(encoder.encode("password"))
+        .roles("ADMIN")
+        .build();
+    return new InMemoryUserDetailsManager(admin);
+  }
+//============================= ===================================================================
+  @Bean
+  SecurityFilterChain           securityFilterChain(HttpSecurity http) throws Exception {
+    http
+      .authorizeHttpRequests((authorize) -> authorize
+        .anyRequest()
+        .authenticated()
+      )
+      .csrf(AbstractHttpConfigurer::disable)
+      .httpBasic(Customizer.withDefaults())
+      .formLogin(Customizer.withDefaults());
+      return http.build();
+  }
+//============================= ===================================================================
+  @Bean
+  PasswordEncoder               passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+}
+
+    
